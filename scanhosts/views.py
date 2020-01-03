565ba1eed543588897cef1e3e7dfebe3f3d83af2
Guoyinzhao <guoyinzhao@qq.com>
2020-01-03 23:30:57 +0800

    change views

--- scanhosts/views.py
+++ scanhosts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse,HttpResponse
+from models import *
+import json
+
+def user_info(request):
+    ip_addr = request.META['REMOTE_ADDR']
+    user_ua = request.META['HTTP_USER_AGENT']
+
+    user_obj = UserIPInfo.object.filter(ip =  ip_addr)
+    if not user_obj:
+        res = UserIPInfo.object.create(ip = ip_addr)
+        ip_add_id = res.id
+    else:
+        ip_add_id = user_obj[0].id
+    BrowseInfo.objects.create(useragent = user_ua,userip_id = ip_add_id)
+
+    result = {"STATUS":"success",
+              "INFO":"User info",
+              "IP":ip_addr,
+              "UA":user_ua}
